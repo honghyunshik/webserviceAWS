3757ab5746716dcfa9c00a60360f7f98140f9e47
hshong1119 <hshong0907@gmail.com>
2023-03-07 19:28:35 +0900

    20230307

--- src/main/java/org/example/service/posts/PostsService.java
+++ src/main/java/org/example/service/posts/PostsService.java
@@ -1,8 +1,11 @@
 package org.example.service.posts;
 
 import lombok.RequiredArgsConstructor;
+import org.example.domain.posts.Posts;
 import org.example.domain.posts.PostsRepository;
+import org.example.web.dto.PostsResponseDto;
 import org.example.web.dto.PostsSaveRequestDto;
+import org.example.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +20,18 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());       //entity 객체만 변경하면 트랜잭션이 끝나는 대로 해당 테이블에 반영문 반영
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 
 }
